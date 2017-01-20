38da44f86912c923a42a229fc05d6c04705e0b2b
Kelvin Jones <kelvin@brightmachine.co.uk>
2017-01-20 20:40:26 +0000

    Remove attempt at `->toArray()` on passed object
    Tried to be too clever in the past by calling `->toArray()` on objects passed in the view data if the method existed.
    
    it never really worked well (not being a deep traversal) and added a tiny bit of unnecessary magic, which was confusing as hell if stepping back into a project that used this package.

--- src/FilesystemLoader.php
+++ src/FilesystemLoader.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Laratash;
 
 use Illuminate\Filesystem\Filesystem;
@@ -15,8 +16,8 @@ class FilesystemLoader implements Mustache_loader
 
     public function __construct(Filesystem $files)
     {
-        $this->app    = app();
-        $this->files  = $files;
+        $this->app = app();
+        $this->files = $files;
         $this->finder = $this->app['view']->getFinder();
     }
 
@@ -25,12 +26,14 @@ class FilesystemLoader implements Mustache_loader
         if (!isset($this->templates[$name])) {
             $this->templates[$name] = $this->loadFile($name);
         }
+
         return $this->templates[$name];
     }
 
     public function loadFile($name)
     {
         $path = $this->finder->find($name);
+
         return $this->files->get($path);
     }
 }
